43810f413d6520978028ae6519efcb4c5dd7472d
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-03-18 16:47:05 -0300

    Adicionando javascripit dark theme e finalizando light theme

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggle');
+const background = document.getElementById('corpo');
+const containerHeader = document.getElementById('header-bg');
+const h1Text = document.getElementById('h1-dark');
+
+toggleButton.addEventListener('click', function () {
+   background.classList.toggle('dark-background');
+   background.classList.toggle('dark-card');
+   background.classList.toggle('dark-main');
+   background.classList.toggle('dark-footer');
+   containerHeader.classList.toggle('dark-header');
+   h1Text.classList.toggle('dark-text');
+})
